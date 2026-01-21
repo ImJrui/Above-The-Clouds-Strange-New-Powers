0a59b0f7b3a2d8bd6baf26b99d7b8afed919e349
ImJrui <huangjianrui1997@163.com>
2026-01-21 18:54:54 +0800

    修复专服无法读取配置的bug

--- main/constants.lua
+++ main/constants.lua
@@ -2,16 +2,16 @@ local GetModConfigData = GetModConfigData
 GLOBAL.setfenv(1, GLOBAL)
 
 PL_CONFIG = {
-    PUGALISK_HP_MULTIPLIER = GetModConfigData("PUGALISK_HP_MULTIPLIER"),
-    ANCIENT_HERALD_HP_MULTIPLIER = GetModConfigData("ANCIENT_HERALD_HP_MULTIPLIER"),
-    ANCIENT_HULK_HP_MULTIPLIER = GetModConfigData("ANCIENT_HULK_HP_MULTIPLIER"),
-    ANTQUEEN_HP_MULTIPLIER = GetModConfigData("ANTQUEEN_HP_MULTIPLIER"),
-    ENABLE_SKILLTREE = GetModConfigData("ENABLE_SKILLTREE"),
-    ENABLE_TERRARIUM = GetModConfigData("ENABLE_TERRARIUM"),
-    ENABLE_TOUCHSTONE = GetModConfigData("ENABLE_TOUCHSTONE"),
-    ENABLE_CRITTERLAB = GetModConfigData("ENABLE_CRITTERLAB"),
-    APORKALYPSE_PERIOD_LENGTH = GetModConfigData("APORKALYPSE_PERIOD_LENGTH"),
-    ENABLE_SKYWORTHY = GetModConfigData("ENABLE_SKYWORTHY"),
+    PUGALISK_HP_MULTIPLIER = GetModConfigData("PUGALISK_HP_MULTIPLIER") or GetModConfigData("rate_pugalisk_health"),
+    ANCIENT_HERALD_HP_MULTIPLIER = GetModConfigData("ANCIENT_HERALD_HP_MULTIPLIER") or GetModConfigData("rate_ancient_herald_health"),
+    ANCIENT_HULK_HP_MULTIPLIER = GetModConfigData("ANCIENT_HULK_HP_MULTIPLIER") or GetModConfigData("rate_ancient_hulk_health"),
+    ANTQUEEN_HP_MULTIPLIER = GetModConfigData("ANTQUEEN_HP_MULTIPLIER") or GetModConfigData("rate_antqueen_health"),
+    ENABLE_SKILLTREE = GetModConfigData("ENABLE_SKILLTREE") or GetModConfigData("EnableSkilltree"),
+    ENABLE_TERRARIUM = GetModConfigData("ENABLE_TERRARIUM") or GetModConfigData("Terrarium"),
+    ENABLE_TOUCHSTONE = GetModConfigData("ENABLE_TOUCHSTONE") or GetModConfigData("TouchStone"),
+    ENABLE_CRITTERLAB = GetModConfigData("ENABLE_CRITTERLAB") or GetModConfigData("Critterlab"),
+    APORKALYPSE_PERIOD_LENGTH = GetModConfigData("APORKALYPSE_PERIOD_LENGTH" or GetModConfigData("APORKALYPSE_PERIOD_LENGTH")),
+    ENABLE_SKYWORTHY = GetModConfigData("ENABLE_SKYWORTHY" or GetModConfigData("EnableSkyworthy")),
 }
 
 PL_WORLDTYPE = { -- 键对应它们的levels.location的大写，值对应世界tag的大写
