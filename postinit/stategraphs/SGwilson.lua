fdd7240549b3854d369c284597cf9ba605f8310e
ImJrui <huangjianrui1997@163.com>
2026-03-05 09:43:28 +0800

    修复虚空坠落bug

--- postinit/stategraphs/SGwilson.lua
+++ postinit/stategraphs/SGwilson.lua
@@ -4,6 +4,21 @@ local AddStategraphActionHandler = AddStategraphActionHandler
 local AddStategraphPostInit = AddStategraphPostInit
 GLOBAL.setfenv(1, GLOBAL)
 
+local function ForceStopHeavyLifting(inst)
+    if inst.components.inventory:IsHeavyLifting() then
+        inst.components.inventory:DropItem(
+            inst.components.inventory:Unequip(EQUIPSLOTS.BODY),
+            true,
+            true
+        )
+    end
+end
+
+local function ToggleOffPhysics(inst)
+    inst.sg.statemem.isphysicstoggle = true
+	inst.Physics:SetCollisionMask(COLLISION.GROUND)
+end
+
 AddStategraphPostInit("wilson", function(sg)
     local cower_onenter = sg.states["cower"].onenter
     sg.states["cower"].onenter = function(inst, data)
@@ -35,5 +50,34 @@ AddStategraphPostInit("wilson", function(sg)
             --interrupted
             inst.components.rider:ActualDismount()
         end
+    end
+    -- 修复在虚空坠落的bug
+    local _abyss_fall_onenter = sg.states["abyss_fall"].onenter
+    sg.states["abyss_fall"].onenter = function(inst, ...)
+        if not TheWorld.has_pl_ocean then
+			ForceStopHeavyLifting(inst)
+			inst.components.rider:ActualDismount()
+			inst.components.locomotor:Stop()
+			inst.components.locomotor:Clear()
+			inst:ClearBufferedAction()
+			inst:ShowHUD(false)
+
+			inst.AnimState:PlayAnimation("abyss_fall")
+            if inst.components.drownable then
+                local teleport_x, teleport_y, teleport_z
+                if teleport_pt then
+                    teleport_x, teleport_y, teleport_z = teleport_pt:Get()
+                end
+                inst.components.drownable:OnFallInVoid(teleport_x, teleport_y, teleport_z)
+            end
+			inst.DynamicShadow:Enable(false)
+			ToggleOffPhysics(inst)
+			if inst.components.playercontroller then
+				inst.components.playercontroller:Enable(false)
+			end
+			inst.components.health:SetInvincible(true)
+            return 
+        end
+        return _abyss_fall_onenter(inst, ...)
     end
 end)
