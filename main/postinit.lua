de5047a45ec625dff4d538c66e15f8f45088fa66
ImJrui <huangjianrui1997@163.com>
2026-03-13 00:24:31 +0800

    移除已在ATC修复的问题

--- main/postinit.lua
+++ main/postinit.lua
@@ -35,7 +35,6 @@ local component_posts = {
 }
 
 local component_posts_skyworthy = { -- it takes effect when the 3 shards mode is enabled
-    "moistureoverride",
     "sinkholespawner",
     "acidinfusible",
     "acidlevel",
