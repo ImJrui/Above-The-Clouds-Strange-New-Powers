372262e29426bc7da35f35cc93d105bb3ef003dc
ImJrui <huangjianrui1997@163.com>
2026-06-19 22:17:19 +0800

    更新wx78modules

--- postinit/prefabs/wx78_moduledefs.lua
+++ postinit/prefabs/wx78_moduledefs.lua
@@ -1,112 +1,62 @@
-local ModuleDefs = require("wx78_moduledefs")
-
 GLOBAL.setfenv(1, GLOBAL)
+
+local wx78_moduledefs = require("wx78_moduledefs")
+local module_definitions = wx78_moduledefs.module_definitions
+local AddNewModuleDefinition = wx78_moduledefs.AddNewModuleDefinition
+local GetModuleDefinitionFromNetID = wx78_moduledefs.GetModuleDefinitionFromNetID
+local AddCreatureScanDataDefinition = wx78_moduledefs.AddCreatureScanDataDefinition
+local GetCreatureScanDataDefinition = wx78_moduledefs.GetCreatureScanDataDefinition
+local AddSpecialCreatureScanDataDefinition = wx78_moduledefs.AddSpecialCreatureScanDataDefinition
 
 local EXTRA_DRYRATE = 2
 
 local ATTACH_RADIUS = {
-    [1] = 5,
-    [2] = 10,
+    [1] = 4,
+    [2] = 6,
 }
 
-local function NotifyFogProofChange(ent)
-    local grogginess = ent ~= nil and ent.components ~= nil and ent.components.grogginess or nil
-    if grogginess ~= nil and grogginess.OnFogProofChange ~= nil then
-        grogginess.OnFogProofChange(ent)
-    end
-end
+local function fan_activate(inst, wx)
+    wx._fan_modules = (wx._fan_modules or 0) + 1
 
-local function fan_undryallies(wx)
-    if wx.PorklandRebalance_LastDriedPlayers ~= nil then
-        for _, player in ipairs(wx.PorklandRebalance_LastDriedPlayers) do
-            if player ~= nil and player:IsValid() and player.PorklandRebalance_DryCount ~= nil then
-                player.PorklandRebalance_DryCount = player.PorklandRebalance_DryCount - 1
-                if player.PorklandRebalance_DryCount <= 0 then
-                    player.PorklandRebalance_DryCount = nil
-                    player:RemoveTag("PorklandRebalance_WX_FogImmune_Ally")
-                    NotifyFogProofChange(player)
-                end
+    if not wx.components.upgrademodulebuff then
+        wx:AddComponent("upgrademodulebuff")
+    end
+
+    local data = {
+        radius = ATTACH_RADIUS[wx._fan_modules] or ATTACH_RADIUS[#ATTACH_RADIUS],
+        number = wx._fan_modules,
+        onattachedfn = function(player)
+            if not player:HasTag("porklandrebalance_fan_buff") then
+                player:AddTag("porklandrebalance_fan_buff")
+            end
+        end,
+        ondetachedfn = function(player)
+            if player:HasTag("porklandrebalance_fan_buff") then
+                player:RemoveTag("porklandrebalance_fan_buff")
             end
         end
-    end
-    wx.PorklandRebalance_LastDriedPlayers = {}
-end
+    }
 
-local function fan_dryallies(wx)
-    wx.PorklandRebalance_numfan = wx.PorklandRebalance_numfan or 0
-
-    local fan_count = math.min(wx.PorklandRebalance_numfan, #ATTACH_RADIUS)
-    local x, y, z = wx.Transform:GetWorldPosition()
-    local all_targets = FindPlayersInRange(x, y, z, ATTACH_RADIUS[fan_count] or 0, true)
-
-    local new_lastdried = {}
-    for i = 1, #all_targets do
-        local player = all_targets[i]
-        if player ~= wx and player:IsValid() then
-            player:AddTag("PorklandRebalance_WX_FogImmune_Ally")
-            player.PorklandRebalance_DryCount = (player.PorklandRebalance_DryCount or 0) + 1
-            NotifyFogProofChange(player)
-            new_lastdried[#new_lastdried + 1] = player
-        end
-    end
-
-    fan_undryallies(wx)
-
-    wx.PorklandRebalance_LastDriedPlayers = new_lastdried
-end
-
-local function fan_activate(inst, wx)
-    if wx == nil then
-        return
-    end
-
-    wx.PorklandRebalance_numfan = (wx.PorklandRebalance_numfan or 0) + 1
-    wx:AddTag("PorklandRebalance_WX_FogImmune")
-    NotifyFogProofChange(wx)
-
-    if wx.components.moisture ~= nil then
-        wx.components.moisture.maxDryingRate = wx.components.moisture.maxDryingRate + EXTRA_DRYRATE
-        wx.components.moisture.baseDryingRate = wx.components.moisture.baseDryingRate + EXTRA_DRYRATE
-        inst.PorklandRebalance_MoistureApplied = true
-    end
-
-    if wx.dryallies == nil and wx.DoPeriodicTask ~= nil then
-        wx.dryallies = wx:DoPeriodicTask(1, fan_dryallies)
-    end
-
-    if wx.AddTemperatureModuleLeaning ~= nil then
-        wx:AddTemperatureModuleLeaning(1)
-        inst.PorklandRebalance_TemperatureApplied = true
-    end
+    wx.components.upgrademodulebuff:SetBuffer("porklandrebalance_fan_buff", data)
 end
 
 local function fan_deactivate(inst, wx)
-    if wx == nil then
-        return
+    wx._fan_modules = (wx._fan_modules or 1) - 1
+    if wx._fan_modules <= 0 then
+        wx._fan_modules = nil
     end
 
-    wx.PorklandRebalance_numfan = math.max(0, (wx.PorklandRebalance_numfan or 1) - 1)
-    if wx.PorklandRebalance_numfan == 0 then
-        wx:RemoveTag("PorklandRebalance_WX_FogImmune")
-        NotifyFogProofChange(wx)
-
-        if wx.dryallies ~= nil then
-            fan_undryallies(wx)
-            wx.dryallies:Cancel()
-            wx.dryallies = nil
+    if wx.components.upgrademodulebuff then
+        if wx._fan_modules and wx._fan_modules > 0 then
+            local data = {
+                radius = ATTACH_RADIUS[wx._fan_modules] or ATTACH_RADIUS[#ATTACH_RADIUS],
+                number = wx._fan_modules,
+            }
+            wx.components.upgrademodulebuff:UpdateBuffer("porklandrebalance_fan_buff", data)
+        else
+            wx.components.upgrademodulebuff:RemoveBuffer("porklandrebalance_fan_buff")
         end
     end
-
-    if inst.PorklandRebalance_MoistureApplied and wx.components.moisture ~= nil then
-        wx.components.moisture.maxDryingRate = wx.components.moisture.maxDryingRate - EXTRA_DRYRATE
-        wx.components.moisture.baseDryingRate = wx.components.moisture.baseDryingRate - EXTRA_DRYRATE
-    end
-    inst.PorklandRebalance_MoistureApplied = nil
-
-    if inst.PorklandRebalance_TemperatureApplied and wx.AddTemperatureModuleLeaning ~= nil then
-        wx:AddTemperatureModuleLeaning(-1)
-    end
-    inst.PorklandRebalance_TemperatureApplied = nil
 end
 
 local FAN_MODULE_DATA =
@@ -121,40 +71,18 @@ local FAN_MODULE_DATA =
     overrideuibuild = "wx_circuits_porkland",
     overrideminiuibuild = "pl_status_wx",
 }
+table.insert(module_definitions, FAN_MODULE_DATA)
 
-local function update_filter_tags(wx)
-    local numfilter = math.max(0, wx.PorklandRebalance_numfilter or 0)
-    wx.PorklandRebalance_numfilter = numfilter
+AddCreatureScanDataDefinition("gnat", "porklandrebalance_fan", 1)
 
-    if numfilter > 0 then
-        wx:AddTag("PorklandRebalance_WX_GasMask")
-    else
-        wx:RemoveTag("PorklandRebalance_WX_GasMask")
-    end
-
-    if numfilter >= 2 then
-        wx:AddTag("prevents_hayfever")
-    else
-        wx:RemoveTag("prevents_hayfever")
-    end
-end
+---------------------------------------------------------------
 
 local function filter_activate(inst, wx)
-    if wx == nil then
-        return
-    end
-
-    wx.PorklandRebalance_numfilter = (wx.PorklandRebalance_numfilter or 0) + 1
-    update_filter_tags(wx)
+    wx:AddTag("wx78_filter_module")
 end
 
 local function filter_deactivate(inst, wx)
-    if wx == nil then
-        return
-    end
-
-    wx.PorklandRebalance_numfilter = math.max(0, (wx.PorklandRebalance_numfilter or 1) - 1)
-    update_filter_tags(wx)
+    wx:RemoveTag("wx78_filter_module")
 end
 
 local FILTER_MODULE_DATA =
@@ -169,20 +97,6 @@ local FILTER_MODULE_DATA =
     overrideuibuild = "wx_circuits_porkland",
     overrideminiuibuild = "pl_status_wx",
 }
+table.insert(module_definitions, FILTER_MODULE_DATA)
 
-local function AddModuleDefinition(data)
-    for _, module_def in ipairs(ModuleDefs.module_definitions) do
-        if module_def.name == data.name then
-            return
-        end
-    end
-
-    ModuleDefs.AddNewModuleDefinition(data)
-    table.insert(ModuleDefs.module_definitions, data)
-end
-
-ModuleDefs.AddCreatureScanDataDefinition("gnat", "porklandrebalance_fan", 1)
-ModuleDefs.AddCreatureScanDataDefinition("peagawk", "porklandrebalance_filter", 4)
-
-AddModuleDefinition(FAN_MODULE_DATA)
-AddModuleDefinition(FILTER_MODULE_DATA)
+AddCreatureScanDataDefinition("peagawk", "porklandrebalance_filter", 4)
---
+++ scripts/components/upgrademodulebuff.lua
@@ -0,0 +1,134 @@
+local duration = 1
+
+local UpgradeModuleBuff = Class(function(self, inst)
+    self.inst = inst
+
+	self.buffers = {}
+	self.buffs = {}
+
+	self.task = nil
+	self.freq = 0.5
+
+	self.inst:ListenForEvent("death", function() self:Disable() end)
+	self.inst:ListenForEvent("onremove", function() self:Disable() end)
+end)
+
+function UpgradeModuleBuff:OnUpdate()
+	for buffname, data in pairs(self.buffers) do
+		self:AttachToTargets(buffname, data)
+	end
+
+	for buffname, data in pairs(self.buffs) do
+		self:DoDelta(buffname, self.freq)
+	end
+
+	if next(self.buffers) == nil and next(self.buffs) == nil then
+		self:Disable()
+		self.inst:RemoveComponent("upgrademodulebuff")
+	end
+end
+
+function UpgradeModuleBuff:SetBuffer(buffname, data)
+	self.buffers[buffname] = data
+	self:Enable()
+end
+
+function UpgradeModuleBuff:UpdateBuffer(buffname, data)
+	for k, v in pairs(data) do
+		if self.buffers[buffname] then
+			self.buffers[buffname][k] = v
+		end
+	end
+end
+
+function UpgradeModuleBuff:RemoveBuffer(buffname)
+	self.buffers[buffname] = nil
+end
+
+function UpgradeModuleBuff:RemoveAllBuffers()
+	for buffname in pairs(self.buffers) do
+		self.buffers[buffname] = nil
+	end
+end
+
+function UpgradeModuleBuff:AttachToTargets(buffname, data)
+	local x, y, z = self.inst.Transform:GetWorldPosition()
+	local players = not TheNet:GetPVPEnabled() and FindPlayersInRange(x, y, z, data.radius, true) or {}
+	for i = 1, #players do
+		local player = players[i]
+		if player and player:IsValid() and not player:HasTag("playerghost") then
+			if not player.components.upgrademodulebuff then
+				player:AddComponent("upgrademodulebuff")
+			end
+			if player.components.upgrademodulebuff.buffs[buffname] == nil then
+				player.components.upgrademodulebuff:Attach(buffname, data)
+			else
+				player.components.upgrademodulebuff:Extend(buffname)
+			end
+		end
+	end
+end
+
+function UpgradeModuleBuff:DoDelta(buffname, delta)
+	if self.buffs[buffname] then
+		self.buffs[buffname].timeleft = self.buffs[buffname].timeleft - delta
+		if self.buffs[buffname].timeleft <= 0 then
+			self:OnDetach(buffname)
+		end
+	end
+end
+
+function UpgradeModuleBuff:Attach(buffname, data)
+	self.buffs[buffname] = {
+		timeleft = duration,
+		onattachedfn = data.onattachedfn,
+		ondetachfn = data.ondetachfn,
+	}
+	if data.onattachedfn then
+		data.onattachedfn(self.inst)
+	end
+	self:Enable()
+end
+
+function UpgradeModuleBuff:Extend(buffname)
+	self.buffs[buffname]["timeleft"] = duration
+end
+
+function UpgradeModuleBuff:OnDetach(buffname)
+	if self.buffs[buffname] then
+		self.buffs[buffname].ondetachfn(self.inst)
+	end
+	self.buffs[buffname] = nil
+end
+
+function UpgradeModuleBuff:RemoveAllBuffs()
+	for buffname in pairs(self.buffs) do
+		self:OnDetach(buffname)
+	end
+	self.buffs = {}
+end
+
+function UpgradeModuleBuff:Enable()
+	if self.task == nil then
+		self.task = self.inst:DoPeriodicTask(self.freq, function() self:OnUpdate() end)
+	end
+end
+
+function UpgradeModuleBuff:Disable()
+	if self.task ~= nil then
+		self.task:Cancel()
+		self.task = nil
+	end
+
+	self:RemoveAllBuffers()
+	self:RemoveAllBuffs()
+end
+
+function UpgradeModuleBuff:OnSave()
+    return nil
+end
+
+function UpgradeModuleBuff:OnLoad(data)
+end
+
+return UpgradeModuleBuff
